83df3553d84c42a0ba357f579ce62d43a75dcdbf
DenIvankov <denivankov93@gmail.com>
2026-02-22 22:15:47 +0300

    22_2-02-26

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { wordsStore } from "../store/wordsStore";
 import WordCard from "./words/WordCard";
 import WordsFilters from "./words/WordsFilters";
--- src/components/words/WordCard.tsx
+++ src/components/words/WordCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IconCheck, IconStar } from "@tabler/icons-react";
 import WordAudio from "./WordAudio";
 
